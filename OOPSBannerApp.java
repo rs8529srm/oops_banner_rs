18bdbadd187e9a99d47978ad817091a0577bad6e
Riya <rs8529@srmist.edu.in>
2026-02-17 21:45:48 +0530

    Coded UC2 to Render OOPS as Banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,12 +1,20 @@
-/**
- * OOPSBannerApp
- * UC1: Foundation - Printing literal text.
- * * @author YourName
- * @version 1.0
- */
 public class OOPSBannerApp {
+
+    // Main method to run the banner display
+    // Print each line of the OOPS banner to create the visual effect for the
+    // message "OOPS". Construct each line using string concatenation.
+    // Each line corresponds to a row in the banner for the letters O, O, P, S.
+    // Using individual print statements for each line.
+
     public static void main(String[] args) {
-        // Core Syntax: System.out is the output stream, println is the method
-        System.out.println("OOPS");
+
+        System.out.println(" *****   *****   ******   ***** ");
+        System.out.println("*     * *     * *     * *     **");
+        System.out.println("*     * *     * *     * *      ");
+        System.out.println("*     * *     * ******   ****** ");
+        System.out.println("*     * *     * *              *");
+        System.out.println("*     * *     * *       **     *");
+        System.out.println(" *****   *****  *         ***** ");
+
     }
 }
