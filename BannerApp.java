3e4d8508d839551801966d36342eafc5c66fbc9b
Riya <rs8529@srmist.edu.in>
2026-03-11 13:29:59 +0530

    IMPLEMENTATION OF UC 8 IS COMPLETED

--- BannerApp.java
+++ BannerApp.java
@@ -1,41 +1,55 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class BannerApp {
 
-    public static class CharacterPatternMap {
-        private char character;
-        private String[] pattern;
+    public static Map<Character, String[]> buildCharacterMap() {
 
-        public CharacterPatternMap(char character, String[] pattern) {
-            this.character = character;
-            this.pattern = pattern;
-        }
+        Map<Character, String[]> map = new HashMap<>();
 
-        public char getCharacter() {
-            return character;
-        }
+        map.put('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
 
-        public String[] getPattern() {
-            return pattern;
-        }
+        map.put('P', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
+
+        map.put('S', new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        });
+
+        return map;
     }
 
-    public static String[] getCharacterPattern(CharacterPatternMap[] patterns, char ch) {
-        for (CharacterPatternMap pattern : patterns) {
-            if (pattern.getCharacter() == ch) {
-                return pattern.getPattern();
-            }
-        }
-        return null;
-    }
+    public static void renderBanner(String message, Map<Character, String[]> map) {
 
-    public static void renderBanner(String message, CharacterPatternMap[] patterns) {
         int height = 7;
 
         for (int i = 0; i < height; i++) {
             StringBuilder line = new StringBuilder();
             for (int j = 0; j < message.length(); j++) {
-                String[] charPattern = getCharacterPattern(patterns, message.charAt(j));
-                if (charPattern != null) {
-                    line.append(charPattern[i]).append("  ");
+                String[] pattern = map.get(message.charAt(j));
+                if (pattern != null) {
+                    line.append(pattern[i]).append("  ");
                 }
             }
             System.out.println(line);
@@ -44,42 +58,7 @@ public class BannerApp {
 
     public static void main(String[] args) {
 
-        String[] O = {
-                " ***** ",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *",
-                " ***** "
-        };
-
-        String[] P = {
-                " ***** ",
-                "*     *",
-                "*     *",
-                " ***** ",
-                "*      ",
-                "*      ",
-                "*      "
-        };
-
-        String[] S = {
-                " ***** ",
-                "*      ",
-                "*      ",
-                " ***** ",
-                "      *",
-                "      *",
-                " ***** "
-        };
-
-        CharacterPatternMap[] patterns = {
-                new CharacterPatternMap('O', O),
-                new CharacterPatternMap('P', P),
-                new CharacterPatternMap('S', S)
-        };
-
-        renderBanner("OOPS", patterns);
+        Map<Character, String[]> characterMap = buildCharacterMap();
+        renderBanner("OOPS", characterMap);
     }
 }
